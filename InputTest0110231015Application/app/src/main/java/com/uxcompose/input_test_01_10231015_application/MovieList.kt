5289d7d6e03f7ad362b98f0718392a9f35b0579b
raison00 <feliciachamberlain@gmail.com>
2026-01-14 13:42:10 -0800

    Add files via upload

---
+++ InputTest0110231015Application/app/src/main/java/com/uxcompose/input_test_01_10231015_application/MovieList.kt
@@ -0,0 +1,89 @@
+package com.uxcompose.input_test_01_10231015_application
+
+object MovieList {
+    val MOVIE_CATEGORY = arrayOf(
+        "Bad Bunny",
+        "Big Bad Wolf",
+        "Bad News Bears",
+        "Bad Dreams",
+        "Bad to the Bone",
+        "Bad Luck"
+    )
+
+    val list: List<Movie> by lazy {
+        setupMovies()
+    }
+    private var count: Long = 0
+
+    private fun setupMovies(): List<Movie> {
+        val title = arrayOf(
+            "Bad Bunny A Year in Review",
+            "Bad Bunny times 2 for the Super Bowl",
+            "Bad Bunny Political Pivot",
+            "Bad Bunny 200M PR Boost",
+            "Bad Bunny Global Takeover"
+        )
+
+        val description = "Bad Bunny A Year in Review "
+        val studio = arrayOf(
+            "Big Bad Wolf",
+            "Bad News Bears",
+            "Bad Dreams",
+            "Bad to the Bone",
+            "Bad Books"
+        )
+        val videoUrl = arrayOf(
+            "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+            "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4",
+            "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4",
+            "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4",
+            "https://commondatastorage.googleapis.com/android-tv/Sample%20videos/April%20Fool's%202013/Introducing%20Google%20Nose.mp4"
+        )
+        val bgImageUrl = arrayOf(
+            "https://feliciachamberlain.com/2026/android/bbbbg.png",
+            "https://feliciachamberlain.com/2026/android/2bbbbg.png",
+            "https://commondatastorage.googleapis.com/android-tv/Sample%20videos/April%20Fool's%202013/Introducing%20Gmail%20Blue/bg.jpg",
+            "https://commondatastorage.googleapis.com/android-tv/Sample%20videos/April%20Fool's%202013/Introducing%20Google%20Fiber%20to%20the%20Pole/bg.jpg",
+            "https://commondatastorage.googleapis.com/android-tv/Sample%20videos/April%20Fool's%202013/Introducing%20Google%20Nose/bg.jpg"
+        )
+        val cardImageUrl = arrayOf(
+            "https://commondatastorage.googleapis.com/android-tv/Sample%20videos/Zeitgeist/Zeitgeist%202010_%20Year%20in%20Review/card.jpg",
+            "https://commondatastorage.googleapis.com/android-tv/Sample%20videos/Demo%20Slam/Google%20Demo%20Slam_%2020ft%20Search/card.jpg",
+            "https://commondatastorage.googleapis.com/android-tv/Sample%20videos/April%20Fool's%202013/Introducing%20Gmail%20Blue/card.jpg",
+            "https://commondatastorage.googleapis.com/android-tv/Sample%20videos/April%20Fool's%202013/Introducing%20Google%20Fiber%20to%20the%20Pole/card.jpg",
+            "https://commondatastorage.googleapis.com/android-tv/Sample%20videos/April%20Fool's%202013/Introducing%20Google%20Nose/card.jpg"
+        )
+
+        val list = title.indices.map {
+            buildMovieInfo(
+                title[it],
+                description,
+                studio[it],
+                videoUrl[it],
+                cardImageUrl[it],
+                bgImageUrl[it]
+            )
+        }
+
+        return list
+    }
+
+    private fun buildMovieInfo(
+        title: String,
+        description: String,
+        studio: String,
+        videoUrl: String,
+        cardImageUrl: String,
+        backgroundImageUrl: String
+    ): Movie {
+        val movie = Movie()
+        movie.id = count++
+        movie.title = title
+        movie.description = description
+        movie.studio = studio
+        movie.cardImageUrl = cardImageUrl
+        movie.backgroundImageUrl = backgroundImageUrl
+        movie.videoUrl = videoUrl
+        return movie
+    }
+}
---
+++ InputTest0110231015Application/app/src/main/java/com/uxcompose/input_test_01_10231015_application/PlaybackActivity.kt
@@ -0,0 +1,19 @@
+package com.uxcompose.input_test_01_10231015_application
+
+import android.os.Bundle
+import androidx.fragment.app.FragmentActivity
+
+/** Loads [PlaybackVideoFragment]. */
+class PlaybackActivity : FragmentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(android.R.id.content, PlaybackVideoFragment())
+                .commit()
+        }
+    }
+
+}
+
