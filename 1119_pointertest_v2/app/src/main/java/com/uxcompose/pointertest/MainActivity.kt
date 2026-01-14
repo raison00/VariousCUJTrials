c7d873bdd78bbd1955605f929e543f0dc149a641
raison00 <feliciachamberlain@gmail.com>
2026-01-14 13:29:37 -0800

    Add files via upload

---
+++ 1119_pointertest_v2/app/src/main/java/com/uxcompose/pointertest/MainActivity.kt
@@ -0,0 +1,161 @@
+package com.uxcompose.pointertest
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+//import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clipToBounds
+import androidx.compose.ui.graphics.Color
+//import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import kotlin.math.roundToInt
+
+class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+    @OptIn(ExperimentalMaterial3Api::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ClippedArea()
+            // Creating a Simple Scaffold
+            // Layout updates needed to add boundaries, will revisit later
+            // https://developer.android.com/develop/ui/compose/system/setup-e2e
+            Scaffold(
+
+                // Creating a Top Bar - to do- add some color so the bar is obvious
+                //notice how the top and bottom bar have different methods for the UI
+                //using https://developer.android.com/develop/ui/compose/designsystems/material3
+                topBar = {
+                    TopAppBar(
+                        title = {
+                            Text(
+                                text = "Pointer Input Test",
+                                fontSize = 48.sp,
+                                fontWeight = FontWeight.Bold
+                                //titleContentColor = androidx.material3.MaterialTheme.colorScheme.onPrimaryContainer
+                            )
+                        },
+                        // Set the colors for the entire app bar
+                        colors = TopAppBarDefaults.topAppBarColors(
+                            containerColor = Color.DarkGray,
+                            titleContentColor = Color.Black
+                        ),
+                        // Optional: Add a Modifier if you need padding or other adjustments for the TopAppBar itself
+                        // modifier = Modifier.padding(horizontal = 8.dp)
+                    )
+                },
+
+
+                //Creating a bottom bar as a test too why do they have different methods to get the UI in?
+                bottomBar = {
+                    BottomAppBar(containerColor = Color.DarkGray) {
+                        Text(
+                            text = "Bottom Bar",
+                            color = Color.Black,
+                            modifier = Modifier.padding(start = 16.dp),
+                            fontSize = 48.sp,
+                            fontWeight = FontWeight.ExtraBold,
+                            maxLines = 1,
+                            softWrap = false,
+                            overflow = TextOverflow.Ellipsis
+                        )
+                    }
+                },
+
+// Creating Content
+                content = { innerPadding ->
+
+                    //    Apply the Scaffold's padding here.
+                    Surface(
+                        modifier = Modifier
+                            .padding(innerPadding) // <-- Use the padding from Scaffold
+                            .fillMaxSize(),
+                        //shape = Shape.Companion.clipToBounds() as Shape,
+                        color = Color.Black // Set the background color on the Surface
+                    ) { // 2. Provide the content for the Surface in its trailing lambda.
+
+                        // 3. Place the Column *inside* the Surface. It should have boundaries so it doesn't get lost or run off the screen.
+                        //do this next- restrict viewport to a constrained area for this exercise.
+
+                        Box(
+                            modifier = Modifier
+                                .size(100.dp, 100.dp) // Define the size of the draggable area.
+                                .border(2.dp, Color.Cyan) // Add a border to see the bounds.
+                                .clipToBounds() // supposed to prevent children from drawing outside.
+                        ){
+
+                            // Offset Values for X and Y
+                            var offsetX by remember { mutableFloatStateOf(0f) }
+                            var offsetY by remember { mutableFloatStateOf(0f) }
+
+                            // click and drag Pointer
+                            Box(
+                                Modifier
+                                    .offset {
+                                        IntOffset(
+                                            offsetX.roundToInt(),
+                                            offsetY.roundToInt()
+                                        )
+                                    }
+                                    .background(Color.Magenta)
+                                    .size(40.dp)
+                                    .pointerInput(Unit) {
+                                        detectDragGestures { change, dragAmount ->
+                                            change.consume()
+                                            offsetX += dragAmount.x
+                                            offsetY += dragAmount.y
+                                        }
+                                    }
+                            )
+                        }
+                    }
+                }    )
+        }
+    }
+
+}
+
+@Composable
+private fun ClippedArea()  {
+    Box(
+        modifier = Modifier
+            .size(200.dp) //  fixed size of the bounding area ( 200x200)
+            .border(2.dp, Color.Cyan) // Visual boundary for clarity
+            .clipToBounds() // ⬅️ THIS IS THE KEY MODIFIER
+    ) {
+        // This Text composable tries to draw partially outside the Box
+        Text(
+            text = "I am clipped!",
+            color = Color.Blue,
+            modifier = Modifier
+                .offset(x = 100.dp, y = 150.dp) // Moves the text far right/down
+                .background(Color.Yellow)
+        )
+    }
+
+    //return
+}
